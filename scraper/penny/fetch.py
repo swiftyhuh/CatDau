60284dd5407d5bc07046393d6cb6a9db7508443c
ImSwifty26 <szabomihau@gmail.com>
2026-08-24 13:06:03 +0300

    added link padding for 0-9 weeks

--- scraper/penny/fetch.py
+++ scraper/penny/fetch.py
@@ -3,4 +3,4 @@ from datetime import date
 todayWeek = date.today().isocalendar().week
 todayYear = date.today().isocalendar().year
 
-URL=f"https://files.rewe.co.at/PennyIntLeaflet/RO/Pliant_National_KW{todayWeek}_{todayYear}/files/assets/common/downloads/Pliant%20National.pdf"
+URL=f"https://files.rewe.co.at/PennyIntLeaflet/RO/Pliant_National_KW{todayWeek:02d}_{todayYear}/files/assets/common/downloads/Pliant%20National.pdf"
